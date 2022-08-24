bf6bc4bd574abbdb64db1e7bf42f4d5cd7fee73c
Ju-yeop <dhwnduq12@naver.com>
2022-08-24 21:21:13 +0900

    Make Header Component

--- src/routers/logged-out-router.tsx
+++ src/routers/logged-out-router.tsx
@@ -1,14 +1,18 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Header } from "../components/header";
+import { HomePage } from "../pages/home";
 import { CreateUser } from "../pages/user/createUser";
 import { Login } from "../pages/user/login";
 
 export const LoggedOutRouter = () => {
   return (
     <BrowserRouter>
+      <Header />
       <Routes>
+        <Route path="/" element={<HomePage />} />
         <Route path="/createuser" element={<CreateUser />} />
-        <Route path="/" element={<Login />} />
+        <Route path="/login" element={<Login />} />
       </Routes>
     </BrowserRouter>
   );
