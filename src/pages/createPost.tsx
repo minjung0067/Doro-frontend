c0d4cc4910381fbca8659c34756a08fce1ed28fc
weightsforfun <giyoung914@gmail.com>
2022-08-25 20:27:01 +0900

    finished create post

--- src/pages/createPost.tsx
+++ src/pages/createPost.tsx
@@ -3,6 +3,7 @@ import React, { useEffect } from "react";
 import { useForm } from "react-hook-form";
 import { useNavigate } from "react-router-dom";
 import { isLoggedInVar } from "../apollo";
+import { Button } from "../components/button";
 import { useMe } from "../hooks/useMe";
 import { createPost, createPostVariables } from "../__generated__/createPost";
 
@@ -18,7 +19,7 @@ const CREATE_POST_MUTATION = gql`
 interface ICreatePostForm {
   title: string;
   content: string;
-  password: string;
+  password?: string;
   ownerName: string;
   institution: string;
   phoneNumber: string;
@@ -27,73 +28,157 @@ interface ICreatePostForm {
 
 export const CreatePost = () => {
   const isLoggedIn = useReactiveVar(isLoggedInVar);
-  if (isLoggedIn) {
-  }
+
   const { data: userData, refetch } = useMe();
-  console.log(userData?.me.email);
   const navigate = useNavigate();
   const { register, formState, getValues, handleSubmit } =
     useForm<ICreatePostForm>({
       mode: "onChange",
-      defaultValues: {
-        ownerName: userData?.me.name,
-        institution: userData?.me.institution,
-        phoneNumber: userData?.me.phoneNumber,
-        email: userData?.me.email,
-      },
     });
+  const onCompleted = (data: createPost) => {
+    const {
+      createPost: { ok, error },
+    } = data;
+    if (ok) {
+      navigate("/posts", { replace: true });
+    } else {
+      console.log(error);
+    }
+  };
   const [createPostMutation, { data: creataPostData, loading }] = useMutation<
     createPost,
     createPostVariables
-  >(CREATE_POST_MUTATION);
+  >(CREATE_POST_MUTATION, { onCompleted });
   const onSubmit = () => {
-    const { ownerName, institution, phoneNumber, email, title, content } =
-      getValues();
-  };
-
-  const onCompleted = (data: createPost) => {
     const {
-      createPost: { ok },
-    } = data;
-    if (ok) {
-    }
+      ownerName,
+      institution,
+      phoneNumber,
+      email,
+      title,
+      content,
+      password,
+    } = getValues();
+    createPostMutation({
+      variables: {
+        input: {
+          ownerName,
+          institution,
+          phoneNumber,
+          email,
+          title,
+          content,
+          password,
+        },
+      },
+    });
   };
 
   return (
     <div>
-      <form onSubmit={handleSubmit(onSubmit)}>
-        <input
-          {...register("ownerName", { required: true })}
-          name="ownerName"
-          placeholder="ownerName"
-        />
-        <input {...register("institution")} name="institution" />
-        <input
-          {...register("phoneNumber", { required: true })}
-          name="phoneNumber"
-          placeholder="phoneNumber"
-        />
-        <input
-          {...register("email", { required: true })}
-          name="email"
-          placeholder="email"
-        />
-        <input
-          {...register("password")}
-          name="password"
-          placeholder="password"
-        />
-        <input
-          {...register("title", { required: true })}
-          name="title"
-          placeholder="title"
-        />
-        <input
-          {...register("content", { required: true })}
-          name="content"
-          placeholder="content"
-        />
-      </form>
+      {isLoggedIn ? (
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <input
+            {...register("ownerName")}
+            name="ownerName"
+            placeholder="ownerName"
+            defaultValue={userData?.me.name ? userData?.me.name : ""}
+          />
+          <input
+            {...register("institution")}
+            name="institution"
+            placeholder="institution"
+            defaultValue={
+              userData?.me.institution ? userData?.me.institution : ""
+            }
+          />
+          <input
+            {...register("phoneNumber")}
+            name="phoneNumber"
+            placeholder="phoneNumber"
+            defaultValue={
+              userData?.me.institution ? userData?.me.institution : ""
+            }
+          />
+          <input
+            {...register("email")}
+            name="email"
+            placeholder="email"
+            defaultValue={userData?.me.email ? userData?.me.email : ""}
+          />
+          <input
+            {...register("password")}
+            name="password"
+            placeholder="password"
+          />
+          <input
+            {...register("title", { required: true })}
+            name="title"
+            placeholder="title"
+          />
+          <input
+            {...register("content", { required: true })}
+            name="content"
+            placeholder="content"
+          />
+          <Button
+            canClick={formState.isValid}
+            loading={loading}
+            actionText={"게시물 등록"}
+          />
+        </form>
+      ) : (
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <input
+            {...register("ownerName", { required: true })}
+            name="ownerName"
+            placeholder="ownerName"
+            defaultValue={userData?.me.name ? userData?.me.name : ""}
+          />
+          <input
+            {...register("institution", { required: true })}
+            name="institution"
+            placeholder="institution"
+            defaultValue={
+              userData?.me.institution ? userData?.me.institution : ""
+            }
+          />
+          <input
+            {...register("phoneNumber", { required: true })}
+            name="phoneNumber"
+            placeholder="phoneNumber"
+            defaultValue={
+              userData?.me.institution ? userData?.me.institution : ""
+            }
+          />
+          <input
+            {...register("email", { required: true })}
+            name="email"
+            placeholder="email"
+            defaultValue={userData?.me.email ? userData?.me.email : ""}
+          />
+          <input
+            {...register("password")}
+            name="password"
+            placeholder="password"
+          />
+          <input
+            {...register("title", { required: true })}
+            name="title"
+            placeholder="title"
+          />
+          <input
+            {...register("content", { required: true })}
+            name="content"
+            placeholder="content"
+          />
+          <Button
+            canClick={formState.isValid}
+            loading={loading}
+            actionText={"게시물 등록"}
+          />
+        </form>
+      )}
     </div>
   );
 };
