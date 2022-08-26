03664c4c4911e476b2da86c7ee422b707d100b0b
Ju-yeop <dhwnduq12@naver.com>
2022-08-26 13:49:01 +0900

    Fix Header li Css

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -20,7 +20,7 @@ export const Header: React.FC = () => {
               <li>
                 <Link
                   to="/posts"
-                  className="block py-2 pr-4 pl-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white"
+                  className="block py-2 pr-4 pl-3 font-bold text-gray-600 md:text-gray-600 md:p-0 dark:text-white hover:text-gray-500"
                   aria-current="page"
                 >
                   커뮤니티
@@ -29,7 +29,7 @@ export const Header: React.FC = () => {
               <li>
                 <Link
                   to="/createPost"
-                  className="block py-2 pr-4 pl-3 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+                  className="block py-2 pr-4 pl-3 font-bold text-gray-600 hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-gray-500 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
                 >
                   교육문의
                 </Link>
