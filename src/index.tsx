4948c7287d1473173313cb92b06f09837e69bac9
Ju-yeop <dhwnduq12@naver.com>
2022-08-28 22:40:31 +0900

    Merge

--- src/index.tsx
+++ src/index.tsx
@@ -6,11 +6,10 @@ import { client } from "./apollo";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import "./styles/styles.css";
-<<<<<<< HEAD
 import "./styles/postsStyles.css";
 import "./styles/commonStyles.css";
-=======
->>>>>>> 08ad24f660daaf67327093d663960e76aa6860e6
+import "./styles/createStyles.css";
+import "./styles/postStyles.css";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
